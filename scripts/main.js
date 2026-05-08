0d19f89ea82c5ac57954cae12c8528fa54385e66
jmichelmuniz <jmichelmuniz@gmail.com>
2026-05-07 21:06:46 -0300

    v0.1.03

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
  var navHeader = document.getElementById('nav-header');
+ var corpo = document.getElementById('corpo');
  var showSidebar = false;
 
  function toggleBar() {
     showSidebar = !showSidebar;
     if (showSidebar) {
         navHeader.style.marginLeft = '0';
+        corpo.style.filter = 'blur(1px)';
     } else {
         navHeader.style.marginLeft = '-100vw';
+        corpo.style.filter = 'none';
     }
- };
+ }
+
+ function closeBar() {
+    if (window.innerWidth < 630 && showSidebar) {
+        toggleBar();
+    }
+ }
+
+ window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 630 && showSidebar){
+        navHeader.style.marginLeft = '0';
+        corpo.style.filter = 'none';
+    }
+ });
