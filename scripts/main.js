9bdc191253ea594b8f7dab1e13e99b30f094fdc0
jmichelmuniz <jmichelmuniz@gmail.com>
2026-05-11 13:32:11 -0300

    v0.2.01

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,11 @@
- var navHeader = document.getElementById('nav-header');
- var corpo = document.getElementById('corpo');
- var showSidebar = false;
+var navbar = document.getElementById('navbar');
+var sidebar = false;
 
- function toggleBar() {
-    showSidebar = !showSidebar;
-    if (showSidebar) {
-        navHeader.style.marginLeft = '0';
-        corpo.style.filter = 'blur(1px)';
-    } else {
-        navHeader.style.marginLeft = '-100vw';
-        corpo.style.filter = 'none';
+function showSidebar() {
+    sidebar = !sidebar;
+    if (sidebar) {
+        navbar.classList.add('active');
+        } else {
+            navbar.classList.remove('active');
+        }
     }
- }
-
- function closeBar() {
-    if (window.innerWidth < 630 && showSidebar) {
-        toggleBar();
-    }
- }
-
- window.addEventListener('resize', function(event) {
-    if(window.innerWidth >= 630){
-        navHeader.style.marginLeft = '0';
-        corpo.style.filter = 'none';
-    }
- });
